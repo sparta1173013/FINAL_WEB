9db8118c19a1dc4f56320a2b814f69301c19f646
Esteban David Ramirez Maradiaga <sparta117naruto@gmail.com>
2019-12-12 21:09:28 -0600

    Creacion Del proyecto

---
+++ WebApplication1/WebApplication1/Modelos/Prueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Modelos
+{
+    public class Prueba
+    {
+        //esta es una clas para crear el royecto se puede eliminar
+    }
+}
